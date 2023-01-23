cb658a83a8523b6c343d9912d0e94a5bb3020b87
Nurdin <@n.ulanuulu>
2023-01-23 14:42:12 +0600

    add routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { routes } from './routes/index.js';
 
 const App = () => (
-    <>
-    </>
+    <Routes>
+        {
+            routes.map((route, index) => {
+                return <Route key={ index } { ...route }/>;
+            })
+        }
+    </Routes>
 );
 
 export default App;
